e3a513afe65eafde45e1318ec0174e51d8befdbf
gitlost <martinb2@online.ie>
2016-05-18 18:28:55 +0100

    Merge fixes from @nicolas-grekas

--- Symfony/Normalizer.php
+++ Symfony/Normalizer.php
@@ -44,23 +44,16 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
     private static $ulenMask = array("\xC0" => 2, "\xD0" => 2, "\xE0" => 3, "\xF0" => 4);
     private static $ASCII = "\x20\x65\x69\x61\x73\x6E\x74\x72\x6F\x6C\x75\x64\x5D\x5B\x63\x6D\x70\x27\x0A\x67\x7C\x68\x76\x2E\x66\x62\x2C\x3A\x3D\x2D\x71\x31\x30\x43\x32\x2A\x79\x78\x29\x28\x4C\x39\x41\x53\x2F\x50\x22\x45\x6A\x4D\x49\x6B\x33\x3E\x35\x54\x3C\x44\x34\x7D\x42\x7B\x38\x46\x77\x52\x36\x37\x55\x47\x4E\x3B\x4A\x7A\x56\x23\x48\x4F\x57\x5F\x26\x21\x4B\x3F\x58\x51\x25\x59\x5C\x09\x5A\x2B\x7E\x5E\x24\x40\x60\x7F\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 	private static $s, $form, $normalize; // gitlost Cache of call to normalize() in isNormalized().
-	private static $mb_overload_string; // gitlost 
 
-	// gitlost begin
     public static function isNormalized($s, $form = self::NFC)
     {
-		if ($form <= self::NONE || $form > self::FORM_KC) { // Note fails if given NONE.
+        if ($form <= self::NONE || self::NFKC < $form) {
 			return false;
 		}
-
-		// Check for multibyte string function overload.
-		if (null === self::$mb_overload_string) {
-			self::$mb_overload_string = defined( 'MB_OVERLOAD_STRING' ) && ( ini_get( 'mbstring.func_overload' ) & MB_OVERLOAD_STRING ); // MB_OVERLOAD_STRING defined by mbstring extension.
-		}
-        if (strspn($s .= '', self::$ASCII) === (self::$mb_overload_string ? mb_strlen($s, '8bit') : strlen($s))) {
+        if (!isset($s[strspn($s .= '', self::$ASCII)])) {
             return true;
         }
-
+		// gitlost begin
         if (self::NFC === $form) { // if (self::NFC === $form && preg_match('//u', $s) && !preg_match('/[^\x00-\x{2FF}]/u', $s)) {
 			if (!tln_is_valid_utf8($s)) {
 				return false;
@@ -83,13 +76,14 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
 		}
 
         return $result;
+		// gitlost end
     }
-	// gitlost end
 
     public static function normalize($s, $form = self::NFC)
     {
-        switch ($form) {
-            case self::NONE: return tln_is_valid_utf8($s .= '') ? $s : false;
+		// gitlost begin
+        switch ($form) { // Check $form first to avoid call to tln_is_valid_utf8() when using cache.
+            case self::NONE: return tln_is_valid_utf8($s .= '') ? $s : false; // Note must still check validity.
             case self::NFC: $C = true; $K = false; break;
             case self::NFD: $C = false; $K = false; break;
             case self::NFKC: $C = true; $K = true; break;
@@ -97,7 +91,6 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
             default: return false;
         }
 
-		// gitlost begin
 		if (null !== self::$normalize && $s === self::$s && $form === self::$form) { //  Use cache if available.
 			$result = self::$normalize;
 			self::$s = self::$form = self::$normalize = null; // Clear cache (try to keep memory usage to a min).
@@ -107,20 +100,11 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
         if (!tln_is_valid_utf8($s .= '')) { // if (!preg_match('//u', $s .= '')) {
             return false;
         }
+		// gitlost end
 
         if ('' === $s) {
             return '';
         }
-
-		if (null === self::$mb_overload_string) {
-			self::$mb_overload_string = defined( 'MB_OVERLOAD_STRING' ) && ( ini_get( 'mbstring.func_overload' ) & MB_OVERLOAD_STRING );
-		}
-		static $mb_internal_encoding;
-		if (self::$mb_overload_string) { 
-			$mb_internal_encoding = mb_internal_encoding(); // Save existing encoding.
-			mb_internal_encoding('8bit');
-		}
-		// gitlost end
 
         if ($K && null === self::$KD) {
             self::$KD = self::getData('compatibilityDecomposition');
@@ -131,23 +115,24 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
             self::$cC = self::getData('combiningClass');
         }
 
+        if (null !== $mbEncoding = (2 /* MB_OVERLOAD_STRING */ & (int) ini_get('mbstring.func_overload')) ? mb_internal_encoding() : null) {
+            mb_internal_encoding('8bit');
+        }
+
+        $r = self::decompose($s, $K);
+
         if ($C) {
             if (null === self::$C) {
                 self::$C = self::getData('canonicalComposition');
             }
 
-            $result = self::recompose(self::decompose($s, $K)); // gitlost
-        } else { // gitlost
-			$result = self::decompose($s, $K); // gitlost
+            $r = self::recompose($r);
 		}
+        if (null !== $mbEncoding) {
+            mb_internal_encoding($mbEncoding);
+        }
 
-		// gitlost begin
-		if (self::$mb_overload_string) {
-			mb_internal_encoding($mb_internal_encoding); // Restore encoding.
-		}
-
-		return $result;
-		// gitlost end
+		return $r;
     }
 
     private static function recompose($s)
@@ -181,7 +166,7 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
 
                 $result .= $lastUchr;
                 $lastUchr = $s[$i];
-				$lastUcls = 0; // gitlost Reset on starter.
+                $lastUcls = 0;
                 ++$i;
                 continue;
             }
@@ -234,16 +219,15 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
         return $result.$lastUchr.$tail;
     }
 
-    private static function decompose($s, $k) // gitlost $c => $k need to remember arg if recursing.
+    private static function decompose($s, $c)
     {
         $result = '';
-		$recurse = false; // gitlost
 
         $ASCII = self::$ASCII;
         $decompMap = self::$D;
         $combClass = self::$cC;
         $ulenMask = self::$ulenMask;
-        if ($k) { // gitlost
+        if ($c) {
             $compatMap = self::$KD;
         }
 
@@ -257,13 +241,7 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
 
                 if ($c) {
                     ksort($c);
-					// gitlost begin
-					$c = implode('', $c);
-					if (!$recurse && isset($combClass[$c])) {
-						$recurse = true;
-					}
-                    $result .= $c;
-					// gitlost end
+                    $result .= implode('', $c);
                     $c = array();
                 }
 
@@ -277,33 +255,10 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
             $uchr = substr($s, $i, $ulen);
             $i += $ulen;
 
-            if (isset($combClass[$uchr])) {
-                // Combining chars, for sorting
-
-                if (!isset($c[$combClass[$uchr]])) {
-                    $c[$combClass[$uchr]] = '';
-                }
-                $c[$combClass[$uchr]] .= isset($compatMap[$uchr]) ? $compatMap[$uchr] : (isset($decompMap[$uchr]) ? $decompMap[$uchr] : $uchr);
-				$lastReorder = $combClass[$uchr];
-                continue;
-            }
-            if ($c) {
-                ksort($c);
-				// gitlost begin
-				$c = implode('', $c);
-				if (!$recurse && isset($combClass[$c])) {
-					$recurse = true;
-				}
-				$result .= $c;
-				// gitlost end
-                $c = array();
-            }
             if ($uchr < "\xEA\xB0\x80" || "\xED\x9E\xA3" < $uchr) {
                 // Table lookup
 
-                $j = isset($compatMap[$uchr]) ? $compatMap[$uchr] : (isset($decompMap[$uchr]) ? $decompMap[$uchr] : $uchr);
-
-                if ($uchr != $j) {
+                if ($uchr !== $j = isset($compatMap[$uchr]) ? $compatMap[$uchr] : (isset($decompMap[$uchr]) ? $decompMap[$uchr] : $uchr)) {
                     $uchr = $j;
 
                     $j = strlen($uchr);
@@ -328,6 +283,15 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
                         $uchr = substr($uchr, 0, $ulen);
                     }
                 }
+                if (isset($combClass[$uchr])) {
+                    // Combining chars, for sorting
+
+                    if (!isset($c[$combClass[$uchr]])) {
+                        $c[$combClass[$uchr]] = '';
+                    }
+                    $c[$combClass[$uchr]] .= $uchr;
+                    continue;
+                }
             } else {
                 // Hangul chars
 
@@ -343,24 +307,20 @@ if ( ! class_exists( 'TLN_Normalizer' ) ) : class TLN_Normalizer // Allow file t
                         : ("\xE1\x87".chr(0x67 + $j));
                 }
             }
+            if ($c) {
+                ksort($c);
+                $result .= implode('', $c);
+                $c = array();
+            }
 
             $result .= $uchr;
         }
 
         if ($c) {
             ksort($c);
-			// gitlost begin
-			$c = implode('', $c);
-			if (!$recurse && isset($combClass[$c])) {
-				$recurse = true;
-			}
-			$result .= $c;
-			// gitlost end
+            $result .= implode('', $c);
         }
 
-		if ($recurse && $result !== $s) { // gitlost Hack to cater for reordering of the reordered (without recursing infinitely). Probably incorrect.
-			$result = self::decompose($result, $k); // gitlost
-		} // gitlost
         return $result;
     }
 
